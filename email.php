00dc155e00e22d09f53ebb958e180b375a9d9616
pakhreen <kisantamang1997@gmail.com>
2019-07-17 00:45:40 +0545

    email setup working

---
+++ email.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require("includes/index.inc.nav.php");
+if(!isset($_SESSION['username'])){
+		header("location:./login.php?next=email");
+}
+?>
+
+<div class="container" style="margin-top:80px;">
+ <div class="row">
+    <form class="col s12 m12" action="./includes/sendemail.inc.php" method="POST">
+      <div class="row">
+        <div class="input-field col s12 m6">
+				<input id="to" name ="to" type="text" class="validate" value="<?php echo $_GET['to']; ?>">
+		<label>To</label>
+        </div>
+			</div>
+				<div class="row">
+        <div class="input-field col s12 m6">
+				<input id="subject" name="subject" type="text" class="validate" value="<?php echo $_GET['subject']; ?>">
+          <label for="subject">Subject</label>
+        </div>
+			</div>
+				<div class="row">
+        <div class="input-field col s12">
+				<textarea id="textarea1" class="materialize-textarea" name="message"><?php echo $_GET['message']; ?></textarea>
+          <label for="textarea1">Textarea</label>
+        </div>
+<div class="right-align">
+		<a class="btn waves-effect waves-light" href="index.php">Cancel</a>
+		<button class="btn waves-effect waves-light" name="sendEmail">Send<i class="material-icons right">send</i></button>
+</div>
+      </div>
+    </form>
+  </div>
+</div>
+
+<?php include("footer.php"); ?>
